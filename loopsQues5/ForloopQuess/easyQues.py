b884def86c7729fd398bb7c35174db1c7a2dad9b
anushkaahlawat <anushkaahlawat28@gmail.com>
2026-09-12 16:16:13 +0530

    update python question file

--- loopsQues5/ForloopQuess/easyQues.py
+++ loopsQues5/ForloopQuess/easyQues.py
@@ -1,10 +1,100 @@
 # 1.Print numbers from 1 to 10 using a for loop.
+
+
+'''
+for i in range(1,11):
+    print(i)'''
+
+    
+    
 # 2.Print numbers from 10 to 1.
+
+
+'''
+for i in range(11,0,-1):
+    print(i)'''
+
+
+
 # 3.Print all even numbers from 1 to 20.
+
+
+
+
+'''for i in range(2,21,2):
+    print(i)'''
+
+
+
 # 4.Print all odd numbers from 1 to 20.
+
+
+
+'''for i in range(1,21,2):
+    print(i)'''
+
+
+
 # 5.Print numbers from 1 to N, where N is entered by the user.
+
+
+
+'''n = int(input("enter a number :"))
+
+for i in range(1,n):
+    print(i)'''
+
+
+
 # 6.Find the sum of numbers from 1 to 10.
+
+
+
+'''n = 10
+sum = 0
+
+for i in range(1,n+1):
+    sum += i
+print("the sum is",sum)'''
+
+
+    
 # 7.Find the sum of numbers from 1 to N.
+
+
+
+'''x = 15
+total = 0
+
+for i in range(1,x+1):
+    total += i
+print("the total sum is",total)'''
+
+
+
 # 8.Print the multiplication table of a number entered by the user.
+
+
+
+'''n = int(input("enter a number :"))
+
+for i in range(1,11):
+    print(n*i)'''
+
+   
+
 # 9.Print the first 10 multiples of a number.
+
+
+
+'''for i in range(2,22,2):
+    print(i)'''
+
+
+
 # 10.Print "Hello" 5 times using a for loop.
+
+
+
+for i in range(5):
+    print("Hello")
